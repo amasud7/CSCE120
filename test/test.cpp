a6308105091f5cb26b905e758efb970586e70ab9
Ayad Masud <ayad.r.masud@gmail.com>
2024-06-08 11:22:52 -0500

    testing cin for numbers

--- test/test.cpp
+++ test/test.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 
 int main() {
-   int ary[3] = {10, 100, 1000};
-   int* hello;
-   hello = ary;
-   *hello = 7;
-   cout << ary[0] << endl;
-   cout << ary[1];
+   int a = 0 ;
+   int b = 0;
+   cout << "enter 2 number: ";
+   cin >> a >> b;
+   cout << a << " " << b;
+
+   return 0;
 }
