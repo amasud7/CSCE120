173a11e4ceccb4b4cd6151f66c5da2a72f9bf8c5
Ayad Masud <ayad.r.masud@gmail.com>
2024-06-08 20:37:47 -0500

    screened input for valid or invalid inputs

--- mountainValleys/mountains_valleys.cpp
+++ mountainValleys/mountains_valleys.cpp
@@ -4,11 +4,23 @@
 using std::cout, std::cin, std::endl;
 
 int main() {
+	int a = 0;
+	int b = 0;
+	bool pos = false;
 	// TODO(student): print prompt for input
-	
 	// TODO(student): read the numbers from standard input
-
 	// TODO(student): validate input (and reprompt on invalid input)
+	do {
+		cout << "Enter numbers 10 <= a <= b < 10000: ";
+		cin >> a >> b;
+		if ((a >= 0) && (b >= 0)) {
+			pos = true;
+		}
+		// implement alternating digits??
+		if (pos) {
+			cout << "Invalid Input" << endl;
+		}
+	} while (!pos);
 
 	// TODO(student): compute and display solution
 	
